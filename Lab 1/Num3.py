0698bda70200c19e46fa613bbbcb05a8cb29e96e
Ashur Motlagh <ashurmotlagh@gmail.com>
2021-02-11 11:34:37 -0800

    reformatted Num3.py

--- Lab 1/Num3.py
+++ Lab 1/Num3.py
@@ -1,6 +1,7 @@
 import numpy as np
+
 headcnt = 0  # This is my heads counter
-succtest = 0 # this is my counter for the successful tests
+succtest = 0  # this is my counter for the successful tests
 
 for i in range(0, 100000):
     coin = np.random.randint(0, 2, 100)
