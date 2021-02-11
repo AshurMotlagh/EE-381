f11ded6c5b9bfbc03b50105fcc31e299ab31c181
Ashur Motlagh <ashurmotlagh@gmail.com>
2021-02-11 11:06:24 -0800

    updated some variable names and added comments

--- Lab 1/Num2.py
+++ Lab 1/Num2.py
@@ -4,20 +4,19 @@ import matplotlib.pyplot as plt
 
 goal = 7  # goal of this code is to get to the number 7!
 cnt = 0  # this is my counter
-eList = []
+eList = []  # empty list
 
-cnt2 = 0  # this is for tracking the rolls it takes to get to seven
+rollcnt = 0  # this is for tracking the rolls it takes to get to seven
 while cnt < 100000:
     d1 = np.random.randint(1, 7)  # referenced this code from SUM OF THE ROLLS OF TWO FAIR DICE
     d2 = np.random.randint(1, 7)  # referenced this code from SUM OF THE ROLLS OF TWO FAIR DICE
     sum = d1 + d2  # referenced this code from SUM OF THE ROLLS OF TWO FAIR DICE
-    cnt2 += 1
+    rollcnt += 1
     if sum == goal:
-        eList.append(cnt2)
-        cnt2 = 0  # reset the counter
+        eList.append(rollcnt)
+        rollcnt = 0  # reset the counter
     cnt += 1  # this is to break out of the while loop
 
-print(eList)
 b = range(1, 22)
 sb = np.size(b)
 h1, bin_edges = np.histogram(eList, bins=b)
